3bc0286a78e377ec8bfa11c367b502fec0b2cbc2
Mister Souza <mistersouza@icloud.com>
2024-08-21 20:27:48 +0200

    Configure REST framework settings for renderers and filters

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'LittleLemonAPI',
 ]
@@ -123,3 +124,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',  # Optional, if XMLRenderer is installed
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+}
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 from .models import *
 from .serializers import *
 
