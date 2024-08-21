ea8cd93d0f2a4b55b269b6ec579c7a41b1019944
Mister Souza <mistersouza@icloud.com>
2024-08-21 17:25:13 +0200

    Add Category model

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
+
 class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2)
