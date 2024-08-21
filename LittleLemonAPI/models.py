77ac3178e3fd83b433fa32bfd84f2a09099e2acd
Mister Souza <mistersouza@icloud.com>
2024-08-21 18:55:26 +0200

    Map CartViewSet

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -14,7 +14,7 @@ class MenuItem(models.Model):
     
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    menuitem = models.ManyToManyField(MenuItem, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField(),
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     total_price = models.DecimalField(max_digits=6, decimal_places=2)
--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import MenuItemViewSet
+from .views import *
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'menu-items', MenuItemViewSet, basename='menuitems')
+router.register(r'cart', CartViewSet, basename='carts')
 
 urlpatterns = [
     path('', include(router.urls)),
